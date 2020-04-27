b96fe205a564897f7d3bc431606fd77470379626
yrojasf <64239635+yrojasf@users.noreply.github.com>
2020-04-26 23:42:31 -0400

    added new documentation

--- OneLine.Common/Bases/ApiResponsePaged/IApiResponsePaged.cs
+++ OneLine.Common/Bases/ApiResponsePaged/IApiResponsePaged.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 namespace OneLine.Bases
 {
+    /// <summary>
+    /// This interface is a definition of actions based on the state and response result paged of the api 
+    /// </summary>
+    /// <typeparam name="T">The api response paged type</typeparam>
     public interface IApiResponsePaged<T>
     {
         IResponseResult<IApiResponse<IPaged<IEnumerable<T>>>> ResponsePaged { get; set; }
--- OneLine.Common/Bases/BlobData/IBlobData.cs
+++ OneLine.Common/Bases/BlobData/IBlobData.cs
@@ -2,6 +2,10 @@
 
 namespace OneLine.Bases
 {
+    /// <summary>
+    /// This interface is a definition of blob data representation
+    /// </summary>
+    /// <typeparam name="TBlobData">The blob data type</typeparam>
     public interface IBlobData<TBlobData>
     {
         IList<TBlobData> BlobDatas { get; set; }

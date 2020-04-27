9edbc50e0d19531210de41ebe63161fd11ac6269
yrojasf <64239635+yrojasf@users.noreply.github.com>
2020-04-26 21:59:13 -0400

    Added documentation
    IApiResponseable

--- OneLine.Common/Bases/ApiResponseable/IApiResponseable.cs
+++ OneLine.Common/Bases/ApiResponseable/IApiResponseable.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace OneLine.Bases
 {
+    /// <summary>
+	/// This interface is a definition of actions based on the state and response result of the api
+	/// </summary>
+	/// <typeparam name="T">The api response type</typeparam>
     public interface IApiResponseable<T>
     {
         IResponseResult<IApiResponse<T>> Response { get; set; }

5eb49f6bd37cb640edfeba2f2aa4049b834f5e48
Anthony Rivera <anthony_elgueta@hotmail.com>
2020-08-15 21:19:40 -0400

    Removed reflection methods.

--- OneLine.Common/Extensions/ServiceProviderExtensions.cs
+++ OneLine.Common/Extensions/ServiceProviderExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using OneLine.Bases;
 using System;
-using System.Linq;
-using System.Reflection;
 
 namespace OneLine.Extensions
 {
@@ -17,36 +15,6 @@ namespace OneLine.Extensions
             where THttpService : IHttpService
         {
             serviceProvider.GetRequiredService<THttpService>().HttpClient.BaseAddress = baseAddress;
-        }
-        public static void SetHttpClientServiceBaseAddressFromAssembly<THttpService>(this IServiceProvider serviceProvider, Assembly assembly, string baseAddress)
-            where THttpService : IHttpService
-        {
-            foreach(var type in assembly.GetTypes().Where(w => w.IsAssignableFrom(typeof(THttpService))))
-            {
-                ((THttpService)serviceProvider.GetRequiredService(type)).HttpClient.BaseAddress = new Uri(baseAddress);
-            }
-        }
-        public static void SetHttpClientServiceBaseAddressFromAssembly<THttpService>(this IServiceProvider serviceProvider, Assembly assembly, Uri baseAddress)
-            where THttpService : IHttpService
-        {
-            foreach (var type in assembly.GetTypes().Where(w => w.IsAssignableFrom(typeof(THttpService))))
-            {
-                ((THttpService)serviceProvider.GetRequiredService(type)).HttpClient.BaseAddress = baseAddress;
-            }
-        }
-        public static void SetHttpClientServiceBaseAddressFromAssemblyContaining<T>(this IServiceProvider serviceProvider, string baseAddress)
-        {
-            foreach (var type in Assembly.GetAssembly(typeof(T)).GetTypes().Where(w => w.IsAssignableFrom(typeof(IHttpService))))
-            {
-                ((IHttpService)serviceProvider.GetRequiredService(type)).HttpClient.BaseAddress = new Uri(baseAddress);
-            }
-        }
-        public static void SetHttpClientServiceBaseAddressFromAssemblyContaining<T>(this IServiceProvider serviceProvider, Uri baseAddress)
-        {
-            foreach (var type in Assembly.GetAssembly(typeof(T)).GetTypes().Where(w => w.IsAssignableFrom(typeof(IHttpService))))
-            {
-                ((IHttpService)serviceProvider.GetRequiredService(type)).HttpClient.BaseAddress = baseAddress;
-            }
         }
     }
 }

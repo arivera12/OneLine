5bb5cac9a7c84c81a8aced49a7521a264e86da0a
Anthony Rivera <anthony_elgueta@hotmail.com>
2020-09-07 21:15:20 -0400

    Removed ApplictionState and Translator implementations to the output project on blazr. Dropped xamarin stuff.

--- OneLine.Blazor/Services/Translator/ITranslator.cs
+++ OneLine.Blazor/Services/Translator/ITranslator.cs
@@ -1,19 +1,10 @@
 ﻿using JsonLanguageLocalizerNet;
-using Microsoft.JSInterop;
-using System.Globalization;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Xamarin.Essentials.Interfaces;
 
 namespace OneLine.Blazor.Services
 {
     public interface ITranslator
     {
-        HttpClient HttpClient { get; set; }
-        IJSRuntime JSRuntime { get; set; }
-        IJsonLanguageLocalizerService LanguageLocalizer { get; set; }
-        IJsonLanguageLocalizerSupportedCulturesService LanguageLocalizerSupportedCultures { get; set; }
-        ISecureStorage SecureStorage { get; set; }
         Task<JsonLanguageLocalizerService> Translate(string applicationLocale);
         Task<string> GetCurrentApplicationLocale();
     }

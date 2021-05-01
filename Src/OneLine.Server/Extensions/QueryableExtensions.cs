2b41c51ec9fb9f8bba7d312f0799696b09b23bdc
Anthony Rivera <anthony_elgueta@hotmail.com>
2021-05-01 13:34:31 -0400

    Added paging and records searching with count in a single db roundtrip.

--- Src/OneLine.Server/Extensions/QueryableExtensions.cs
+++ Src/OneLine.Server/Extensions/QueryableExtensions.cs
@@ -31,7 +31,7 @@ namespace OneLine.Extensions
             Page = Page.HasValue && Page.Value >= 0 ? Page.Value : 0;
             PageSize = PageSize.HasValue && PageSize.Value > 0 ? PageSize.Value : Count;
 
-            return source.Skip(Page.Value * PageSize.Value).Take(PageSize.Value).AsQueryable();
+            return source.Skip(Page.Value * PageSize.Value).Take(PageSize.Value).Future().AsQueryable();
         }
         /// <summary>
         /// Pageds a <see cref="IQueryable{T}"/> to an <see cref="IApiResponse{IPaged{IEnumerable{T}}}"/>

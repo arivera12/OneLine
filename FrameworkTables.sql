df7426c7364d0043b41bee147e480a196b6f19af
Anthony Rivera <anthony_elgueta@hotmail.com>
2021-07-28 15:35:40 -0400

    Merged and simplified framework tables in one sql script. Added also some framework tables data.

---
+++ FrameworkTables.sql
@@ -0,0 +1,305 @@
+﻿/****** Object:  Table [dbo].[AspNetRoleClaims]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetRoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](4000) NULL,
+	[ClaimValue] [nvarchar](4000) NULL,
+ CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](256) NULL,
+	[NormalizedName] [nvarchar](256) NULL,
+	[ConcurrencyStamp] [nvarchar](4000) NULL,
+ CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](4000) NULL,
+	[ClaimValue] [nvarchar](4000) NULL,
+ CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserLogins](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[ProviderDisplayName] [nvarchar](4000) NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](450) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [nvarchar](450) NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[NormalizedUserName] [nvarchar](256) NULL,
+	[Email] [nvarchar](256) NULL,
+	[NormalizedEmail] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](4000) NULL,
+	[SecurityStamp] [nvarchar](4000) NULL,
+	[ConcurrencyStamp] [nvarchar](4000) NULL,
+	[PhoneNumber] [nvarchar](4000) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[IsLocked] [bit] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[IsOnline] [bit] NULL,
+	[LastTimeOnline] [datetime] NULL,
+	[PreferredCultureLocale] [nvarchar](32) NULL,
+	[IsMobilePhone] [bit] NULL,
+	[TeleCommunicationServiceProviderId] [int] NULL,
+	[RecordRevisions] [int] NULL,
+	[IsDeleted] [bit] NULL,
+	[CreatedBy] [nvarchar](128) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[ModifiedBy] [nvarchar](128) NULL,
+	[ModifiedOn] [datetime] NULL,
+ CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserTokens]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AspNetUserTokens](
+	[UserId] [nvarchar](450) NOT NULL,
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](128) NOT NULL,
+	[Value] [nvarchar](4000) NULL,
+ CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[LoginProvider] ASC,
+	[Name] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AuditTrails]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AuditTrails](
+	[AuditTrailId] [nvarchar](128) NOT NULL,
+	[Action] [nvarchar](512) NULL,
+	[ActionName] [nvarchar](256) NULL,
+	[ControllerName] [nvarchar](128) NULL,
+	[TableName] [nvarchar](128) NULL,
+	[Record] [nvarchar](max) NULL,
+	[Hostname] [nvarchar](128) NULL,
+	[RemoteIpAddress] [nvarchar](64) NULL,
+	[CreatedBy] [nvarchar](128) NULL,
+	[CreatedOn] [datetime] NOT NULL,
+ CONSTRAINT [PK_audittrail_AuditTrailID] PRIMARY KEY CLUSTERED 
+(
+	[AuditTrailId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ExceptionLogs]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ExceptionLogs](
+	[ExceptionLogId] [nvarchar](128) NOT NULL,
+	[HResult] [int] NULL,
+	[HelpLink] [nvarchar](4000) NULL,
+	[InnerException] [nvarchar](4000) NULL,
+	[Message] [nvarchar](4000) NULL,
+	[Source] [nvarchar](4000) NULL,
+	[StackTrace] [nvarchar](4000) NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[CreatedBy] [nvarchar](128) NULL,
+ CONSTRAINT [PK_exceptionlogs_ExceptionLogID] PRIMARY KEY CLUSTERED 
+(
+	[ExceptionLogId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[NotificationMessages]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NotificationMessages](
+	[NotificationMessageId] [nvarchar](128) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+	[Title] [nvarchar](256) NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[IconUri] [nvarchar](512) NULL,
+	[DestinationUri] [nvarchar](256) NULL,
+	[IsReaded] [bit] NULL,
+	[IsDeleted] [bit] NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[CreatedBy] [nvarchar](128) NULL,
+ CONSTRAINT [PK_NotificationMessages] PRIMARY KEY CLUSTERED 
+(
+	[NotificationMessageId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SupportedCultures]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SupportedCultures](
+	[SupportedCultureId] [nvarchar](128) NOT NULL,
+	[Locale] [nvarchar](32) NOT NULL,
+	[Name] [nvarchar](64) NOT NULL,
+	[IsDeleted] [bit] NULL,
+	[CreatedBy] [nvarchar](128) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[ModifiedBy] [nvarchar](128) NULL,
+	[ModifiedOn] [datetime] NULL,
+ CONSTRAINT [PK_SupportedCultures] PRIMARY KEY CLUSTERED 
+(
+	[SupportedCultureId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TeleCommunicationServiceProviders]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TeleCommunicationServiceProviders](
+	[TeleCommunicationServiceProviderId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](64) NOT NULL,
+	[DomainGateway] [nvarchar](64) NOT NULL,
+	[IsDeleted] [bit] NULL,
+	[RecordRevisions] [int] NULL,
+	[CreatedBy] [nvarchar](128) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[LastModifiedBy] [nvarchar](128) NULL,
+	[LastModifiedOn] [datetime] NULL,
+ CONSTRAINT [PK_TeleCommunicationServiceProviders] PRIMARY KEY CLUSTERED 
+(
+	[TeleCommunicationServiceProviderId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserBlobs]    Script Date: 7/28/2021 3:31:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserBlobs](
+	[UserBlobId] [nvarchar](128) NOT NULL,
+	[UserIdentifier] [nvarchar](450) NOT NULL,
+	[ContentDisposition] [nvarchar](128) NULL,
+	[ContentType] [nvarchar](128) NULL,
+	[FileName] [nvarchar](512) NOT NULL,
+	[Name] [nvarchar](512) NULL,
+	[Length] [bigint] NOT NULL,
+	[FilePath] [nvarchar](512) NOT NULL,
+	[TableName] [nvarchar](128) NOT NULL,
+	[IsDeleted] [bit] NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[CreatedBy] [nvarchar](128) NULL,
+ CONSTRAINT [PK_UserBlob] PRIMARY KEY CLUSTERED 
+(
+	[UserBlobId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+GO
+ALTER TABLE [dbo].[AspNetUserClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserClaims] CHECK CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserLogins]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserLogins] CHECK CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]
+GO
---
+++ FrameworkTablesData.sql
@@ -0,0 +1,18 @@
+﻿SET IDENTITY_INSERT [dbo].[TeleCommunicationServiceProviders] ON
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (1, N'AT&T', N'@mms.att.net', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (2, N'Claro', N'@vtexto.com', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (3, N'Comcast', N'@comcastpcs.textmsg.com', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (4, N'Liberty', N'@mms.att.net', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (5, N'Sprint', N'@pm.sprint.com', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (6, N'T-Mobile', N'@tmomail.net', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[TeleCommunicationServiceProviders] ([TeleCommunicationServiceProviderId], [Name], [DomainGateway], [IsDeleted], [RecordRevisions], [CreatedBy], [CreatedOn], [LastModifiedBy], [LastModifiedOn]) VALUES (7, N'Verizon Wireless', N'@vzwpix.com', 0, 0, N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime), N'0', CAST(N'2021-04-08T00:00:00.000' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[TeleCommunicationServiceProviders] OFF
+GO

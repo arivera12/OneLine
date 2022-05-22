9c2d6c60a92df5409922c452d766493960ed4cf6
Anthony Rivera <anthony_elgueta@hotmail.com>
2022-05-22 15:36:36 -0400

    Removed shiny due to inconsistency on the author. Updated some dependency packages.

--- Src/OneLine/Services/Notification/INotification.cs
+++ Src/OneLine/Services/Notification/INotification.cs
@@ -1,11 +1,9 @@
 ﻿using BlazorNotification;
-using Shiny.Notifications;
 
 namespace OneLine.Services
 {
     public interface INotification
     {
         IBlazorNotificationService BlazorNotificationService { get; set; }
-        INotificationManager NotificationManager { get; set; }
     }
 }
--- Src/OneLine/Services/Notification/Notification.cs
+++ Src/OneLine/Services/Notification/Notification.cs
@@ -1,40 +1,14 @@
 ﻿using BlazorNotification;
 using Microsoft.Extensions.DependencyInjection;
-using Shiny.Notifications;
 
 namespace OneLine.Services
 {
     public class Notification : INotification
     {
         public IBlazorNotificationService BlazorNotificationService { get; set; }
-        public INotificationManager NotificationManager { get; set; }
-        public Notification(IDevice device)
-        {
-            if (device.IsXamarinPlatform &&
-                (device.IsiOSDevice || device.IsAndroidDevice || device.IsWindowsOSPlatform) &&
-                !device.IsDesktop)
-            {
-                NotificationManager = Shiny.ShinyHost.Resolve<INotificationManager>();
-            }
-        }
-        public Notification(IBlazorNotificationService blazorNotificationService, IDevice device)
+        public Notification(IBlazorNotificationService blazorNotificationService)
         {
             BlazorNotificationService = blazorNotificationService;
-            if(device.IsXamarinPlatform &&
-                (device.IsiOSDevice || device.IsAndroidDevice || device.IsWindowsOSPlatform) &&
-                !device.IsDesktop)
-            {
-                NotificationManager = Shiny.ShinyHost.Resolve<INotificationManager>();
-            }
-        }
-        public Notification(INotificationManager notificationManager)
-        {
-            NotificationManager = notificationManager;
-        }
-        public Notification(IBlazorNotificationService blazorNotificationService, INotificationManager notificationManager)
-        {
-            BlazorNotificationService = blazorNotificationService;
-            NotificationManager = notificationManager;
         }
     }
     public static partial class ServiceCollectionExtensions

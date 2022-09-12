7d54fa7121b683faf8c1afdd71cba4888917f2af
Anthony Rivera <anthony_elgueta@hotmail.com>
2022-09-12 14:37:27 -0400

    Added Obsolete attribute. TransactionResultApiResponse is deprecated, please use ToApiResponse instead.

--- Src/OneLine/Extensions/IntergerExtensions.cs
+++ Src/OneLine/Extensions/IntergerExtensions.cs
@@ -1,5 +1,6 @@
 using OneLine.Enums;
 using OneLine.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -23,6 +24,7 @@ namespace OneLine.Extensions
         /// <param name="value">The int value</param>
         /// <param name="data">The data of type of <see cref="{T}"/></param>
         /// <returns></returns>
+        [Obsolete("TransactionResultApiResponse is deprecated, please use ToApiResponse instead.")]
         public static IApiResponse<T> TransactionResultApiResponse<T>(this int value, T data)
         {
             var status = value.Succeeded() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
@@ -35,6 +37,7 @@ namespace OneLine.Extensions
         /// <param name="value">The int value</param>
         /// <param name="data">The data of type of <see cref="IEnumerable{T}"/></param>
         /// <returns></returns>
+        [Obsolete("TransactionResultApiResponse is deprecated, please use ToApiResponse instead.")]
         public static IApiResponse<IEnumerable<T>> TransactionResultApiResponse<T>(this int value, IEnumerable<T> data)
         {
             var status = value.Succeeded() && value >= data.Count() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
@@ -49,6 +52,7 @@ namespace OneLine.Extensions
         /// <param name="successMessage">The success message</param>
         /// <param name="errorMessage">The error message</param>
         /// <returns></returns>
+        [Obsolete("TransactionResultApiResponse is deprecated, please use ToApiResponse instead.")]
         public static IApiResponse<T> TransactionResultApiResponse<T>(this int value, T data, string successMessage, string errorMessage)
         {
             var success = value.Succeeded();
@@ -65,12 +69,69 @@ namespace OneLine.Extensions
         /// <param name="successMessage">The success message</param>
         /// <param name="errorMessage">The error message</param>
         /// <returns></returns>
+        [Obsolete("TransactionResultApiResponse is deprecated, please use ToApiResponse instead.")]
         public static IApiResponse<IEnumerable<T>> TransactionResultApiResponse<T>(this int value, IEnumerable<T> data, string successMessage, string errorMessage)
         {
             var success = value.Succeeded();
             var status = success && value >= data.Count() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
             var message = success && value >= data.Count() ? successMessage : errorMessage;
             return new ApiResponse<IEnumerable<T>> (status, data, message);
+        }
+        /// <summary>
+        /// True when a value is greater than zero (0) otherwise false
+        /// </summary>
+        /// <typeparam name="T">The type param</typeparam>
+        /// <param name="value">The int value</param>
+        /// <param name="data">The data of type of <see cref="{T}"/></param>
+        /// <returns></returns>
+        public static IApiResponse<T> ToApiResponse<T>(this int value, T data)
+        {
+            var status = value.Succeeded() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
+            return new ApiResponse<T>(status, data);
+        }
+        /// <summary>
+        /// True when a value is greater than zero (0) otherwise false
+        /// </summary>
+        /// <typeparam name="T">The type param</typeparam>
+        /// <param name="value">The int value</param>
+        /// <param name="data">The data of type of <see cref="IEnumerable{T}"/></param>
+        /// <returns></returns>
+        public static IApiResponse<IEnumerable<T>> ToApiResponse<T>(this int value, IEnumerable<T> data)
+        {
+            var status = value.Succeeded() && value >= data.Count() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
+            return new ApiResponse<IEnumerable<T>>(status, data);
+        }
+        /// <summary>
+        /// True when a value is greater than zero (0) otherwise false
+        /// </summary>
+        /// <typeparam name="T">The type param</typeparam>
+        /// <param name="value">The int value</param>
+        /// <param name="data">The data of type of <see cref="{T}"/></param>
+        /// <param name="successMessage">The success message</param>
+        /// <param name="errorMessage">The error message</param>
+        /// <returns></returns>
+        public static IApiResponse<T> ToApiResponse<T>(this int value, T data, string successMessage, string errorMessage)
+        {
+            var success = value.Succeeded();
+            var status = success ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
+            var message = success ? successMessage : errorMessage;
+            return new ApiResponse<T>(status, data, message);
+        }
+        /// <summary>
+        /// True when a value is greater than zero (0) otherwise false
+        /// </summary>
+        /// <typeparam name="T">The type param</typeparam>
+        /// <param name="value">The int value</param>
+        /// <param name="data">The data of type of <see cref="IEnumerable{T}"/></param>
+        /// <param name="successMessage">The success message</param>
+        /// <param name="errorMessage">The error message</param>
+        /// <returns></returns>
+        public static IApiResponse<IEnumerable<T>> ToApiResponse<T>(this int value, IEnumerable<T> data, string successMessage, string errorMessage)
+        {
+            var success = value.Succeeded();
+            var status = success && value >= data.Count() ? ApiResponseStatus.Succeeded : ApiResponseStatus.Failed;
+            var message = success && value >= data.Count() ? successMessage : errorMessage;
+            return new ApiResponse<IEnumerable<T>>(status, data, message);
         }
     }
 }
